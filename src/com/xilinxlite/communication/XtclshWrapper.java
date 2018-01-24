6f9d5b5b6e68cdc9f836ecaabc7311ebed9ba7f3
holystar92 <C140149@e.ntu.edu.sg>
2018-01-24 16:29:16 +0800

    XtclshWrapper class finished. CMD doesn't seem to be required for this project.

--- src/com/xilinxlite/communication/XtclshWrapper.java
+++ src/com/xilinxlite/communication/XtclshWrapper.java
@@ -5,87 +5,156 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
 /**
- * Wrapper for xtclsh.exe of local copy of Xilinx ISE xtclsh.exe [script]
- * [commands..]
+ * Wrapper for xtclsh.exe of local copy of Xilinx ISE. The executable file runs
+ * with the following format: "xtclsh.exe [script] [commands..]". This wrapper
+ * is built around the three parameters given on construction. After each run(),
+ * both input and error readers must be called again for reading as each run()
+ * builds a new process.
  * 
  * @author Ong Hock Leng
  *
  */
 class XtclshWrapper {
 
+	private static final Logger logger = Logger.getLogger(XtclshWrapper.class.getName());
+
 	private String xtclshPath;
 	private String tclScriptPath;
 	private String workingDirectory;
+	private Process p;
 
 	/**
-	 * Constructor. Throws FileNotFoundException if the parameters are incorrect.
-	 * Message is built for dialog box.
+	 * Constructor hidden from outside use. Validation required before construction.
 	 * 
-	 * @param xtclsh
-	 * @param tclScript
+	 * @param xtclshPath
+	 * @param tclScriptPath
+	 * @param workingDirectory
+	 */
+	private XtclshWrapper(String xtclshPath, String tclScriptPath, String workingDirectory) {
+		this.xtclshPath = xtclshPath;
+		this.tclScriptPath = tclScriptPath;
+		this.workingDirectory = workingDirectory;
+	}
+
+	/**
+	 * Build instance with default working directory.
+	 * 
+	 * @param xtclshPath
+	 * @param tclScriptPath
+	 * @return XtclshWrapper
 	 * @throws FileNotFoundException
 	 */
-	XtclshWrapper(String xtclsh, String tclScript) throws FileNotFoundException {
-		this(xtclsh, tclScript, null);
-	}
-
-	XtclshWrapper(String xtclsh, String tclScript, String workingDirectory) throws FileNotFoundException {
-		xtclshPath = xtclsh;
-		tclScriptPath = tclScript;
-		String errorMsg = "";
-
-		// Check xtclsh.exe
-		if (!xtclshPath.endsWith("xtclsh.exe"))
-			errorMsg += xtclshPath + " is not the correct program.";
-		else if (!new File(xtclshPath).exists())
-			errorMsg += (errorMsg.isEmpty() ? "" : "\n") + xtclshPath + " is not found.";
-
-		// Check TCL script
-		if (!tclScriptPath.endsWith(".tcl"))
-			errorMsg += tclScriptPath + " is not a TCL script.";
-		else if (!new File(tclScriptPath).exists())
-			errorMsg += (errorMsg.isEmpty() ? "" : "\n") + tclScriptPath + " is not found.";
-
-		if (workingDirectory == null) {
-			// Sets default working directory
-			this.workingDirectory = System.getProperty("user.home") + "\\Xilinx_Lite";
-			File file = new File(this.workingDirectory);
-			if (!file.exists())
-				file.mkdirs();
-		} else {
-			// Checks for valid working directory
-			this.workingDirectory = workingDirectory;
-			if (!new File(this.workingDirectory).exists())
-				errorMsg += (errorMsg.isEmpty() ? "" : "\n") + workingDirectory + " is not found.";
-		}
-
-		if (!errorMsg.isEmpty())
-			throw new FileNotFoundException(errorMsg);
+	public static XtclshWrapper getInstance(String xtclshPath, String tclScriptPath) throws FileNotFoundException {
+		String workingDirectory = System.getProperty("user.home") + "\\Xilinx_Lite";
+		return getInstance(xtclshPath, tclScriptPath, workingDirectory);
 	}
 
 	/**
-	 * Parameter 'commands' is the argument array used in conjunction with the TCL
-	 * shell. The TCL shell doesn't have an input stream, therefore this method
-	 * returns only the input stream.
+	 * Validates all three parameters before construction. Throws exception if error
+	 * building.
 	 * 
-	 * @param commands
-	 * @return
-	 * @throws IOException
+	 * @param xtclshPath
+	 * @param tclScriptPath
+	 * @param workingDirectory
+	 * @return XtclshWrapper
+	 * @throws FileNotFoundException
 	 */
-	BufferedReader run(String... commands) throws IOException {
-		String[] commandArray = new String[commands.length + 2];
-		commandArray[0] = xtclshPath;
-		commandArray[1] = tclScriptPath;
-		for (int i = 0; i < commands.length; i++) {
-			commandArray[2 + i] = commands[i];
+	public static XtclshWrapper getInstance(String xtclshPath, String tclScriptPath, String workingDirectory)
+			throws FileNotFoundException {
+		String errorMsg = "";
+
+		// Validate xtclshPath
+		if (!xtclshPath.endsWith("xtclsh.exe")) {
+			errorMsg += "Program file is not xtclsh.exe";
+		} else if (!new File(xtclshPath).exists()) {
+			errorMsg += xtclshPath + " not found.";
 		}
 
-		ProcessBuilder pb = new ProcessBuilder(commandArray);
-		pb.directory(new File(this.workingDirectory));
-		Process p = pb.start();
+		// Validate tclScriptPath
+		if (!tclScriptPath.endsWith(".tcl")) {
+			errorMsg += (errorMsg.isEmpty() ? "" : "\n") + "Script file not TCL file.";
+		} else if (!new File(tclScriptPath).exists()) {
+			errorMsg += (errorMsg.isEmpty() ? "" : "\n") + tclScriptPath + " not found.";
+		}
+
+		// Validate workingDirectory
+		File wd = new File(workingDirectory);
+		if (!wd.exists()) {
+			if (wd.mkdirs()) {
+				logger.info("Working Directory \"" + workingDirectory + "\" created.");
+			} else {
+				errorMsg += (errorMsg.isEmpty() ? "" : "\n") + "Error creating working directory \"" + workingDirectory
+						+ "\".";
+			}
+		}
+
+		// Return instance if valid; else throw exception
+		if (errorMsg.isEmpty()) {
+			return new XtclshWrapper(xtclshPath, tclScriptPath, workingDirectory);
+		} else {
+			throw new FileNotFoundException(errorMsg);
+		}
+	}
+
+	/**
+	 * Accepts String array commands to be run. IOException is thrown if (1)
+	 * xtclsh.exe is not given a valid path, or (2) TCL script path is not valid.
+	 * 
+	 * @param commands
+	 * @throws IOException
+	 */
+	public void run(String... commands) throws IOException {
+		// Build String array for ProcessBuilder use
+		String[] arr = new String[commands.length + 2];
+		arr[0] = xtclshPath;
+		arr[1] = tclScriptPath;
+		for (int i = 0; i < commands.length; i++) {
+			arr[2 + i] = commands[i];
+		}
+
+		ProcessBuilder pb = new ProcessBuilder(arr);
+		pb.directory(new File(workingDirectory));
+		try {
+			p = pb.start();
+		} catch (IOException e) {
+			logger.warning("XtclshWrapper: run error");
+			throw e;
+		}
+	}
+
+	/**
+	 * Gets input reader for current process. Reads outputs as designed in TCL
+	 * script.
+	 * 
+	 * @return
+	 */
+	public BufferedReader getInputReader() {
 		return new BufferedReader(new InputStreamReader(p.getInputStream()));
+	}
+
+	/**
+	 * Gets error reader for current process. Reads outputs as designed by Xilinx
+	 * ISE.
+	 * 
+	 * @return
+	 */
+	public BufferedReader getErrorReader() {
+		return new BufferedReader(new InputStreamReader(p.getErrorStream()));
+	}
+
+	public String getXtclshPath() {
+		return this.xtclshPath;
+	}
+
+	public String getTclScriptPath() {
+		return this.tclScriptPath;
+	}
+
+	public String getWorkingDirectory() {
+		return this.workingDirectory;
 	}
 
 }
